2df606db51a50c68538b4c8eefa0295f603e7732
huynhhongson <hhongson5011@gmail.com>
2025-07-08 14:06:12 +0700

    complete setup FE

---
+++ server/src/server.js
@@ -0,0 +1,15 @@
+const http = require('http')
+
+const app = require('./app')
+
+const PORT = process.env.PORT || 8000
+
+const server = http.createServer(app)
+
+console.log("PORT = ", PORT);
+server.listen(PORT, () => {
+    console.log(`Listening on PORT ${PORT}...`);
+    
+})
+
+
